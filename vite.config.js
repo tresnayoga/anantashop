8160125a485b29857e2b396053dc402e928cac4f
trsna <tresnayoga06@gmail.com>
2024-11-21 20:29:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,         // Pastikan Vite menghasilkan manifest.json
+    outDir: 'public/build', // Pastikan output build ke folder yang benar
+  },
 });
